60def7183c7c63475ede83ea3b1ded633bd06653
99muneeb <muneeb9166@gmail.com>
2026-04-19 16:58:05 +0300

    add file layout.js in app folder

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "HVAC Services Riyadh",
+  description: "AC repair and maintenance",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
